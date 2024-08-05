678d64bbebf660ec290dad08b1960c0bc2b5f1ab
VadymMakohon <errorks7@gmail.com>
2024-08-05 16:20:30 +0200

    adding another variable

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -3,9 +3,13 @@ string myFriendsName;
 // assign a value to the myFriendsName variable
 myFriendsName = "Jannick";
 
+// declare a variable and assign a value to it
+string myFriendsName2 = "Frank";
+
 // use/access the variable
 Console.WriteLine(myFriendsName);
 
+// overwriting the variable value
 myFriendsName = "Frank";
 Console.WriteLine(myFriendsName);
 
