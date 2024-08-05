e6900901511536638048e9d0039dc31a989116ea
VadymMakohon <errorks7@gmail.com>
2024-08-05 15:42:32 +0200

    minor updates

--- HelloWorldApp/obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ HelloWorldApp/obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ac76831ed9fa0768fed3154699cf4153adc828")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
