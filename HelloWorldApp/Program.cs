b0ac76831ed9fa0768fed3154699cf4153adc828
VadymMakohon <errorks7@gmail.com>
2024-08-05 12:40:06 +0200

    added files to the project

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// this is another comment
+
+// This line of code wil print hte line "Hello, Vadym!"
+Console.WriteLine("Hello, Vadym!");
